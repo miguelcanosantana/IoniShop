9373617b6ac8194cba7147ac349c0c6b4c06d180
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2021-02-08 10:28:25 +0100

    Modified Login and add Register.

--- IoniShop/src/app/pages/login/login.page.ts
+++ IoniShop/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  //Variables
+  email: String;
+  password: String;
+
+  constructor(private router: Router) { }
+
 
   ngOnInit() {
   }
+
+
+  //Login and Redirect to Shop Page
+  goToShop() {
+
+  }
+
+
+  //Redirect to Register Page
+  goToRegister() {
+    this.router.navigateByUrl("/register")
+  }
+
+
 
 }
